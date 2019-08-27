5839613b0c6422f67d7e758821822dfbb78152b0
Jørgen H. Fjeld <jorgen@veridit.no>
2019-08-27 22:17:30 +0200

    Upgrade syntax as required

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const merge = require('webpack-merge')
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const outputDirectory = 'wwwroot'
 
 // to extract the css as a separate file
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 var MODE =
-  process.env.npm_lifecycle_event === 'prod' ? 'production' : 'development'
+    process.env.npm_lifecycle_event === 'prod' ? 'production' : 'development'
 var filename = MODE === 'production' ? '[name]-[hash].js' : 'index.js'
 
 var common = {
